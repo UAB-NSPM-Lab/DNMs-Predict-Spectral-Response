5f3232a367979ddf8b2916202167b70105c223e5
hbriny99 <hbriny99@uab.edu>
2026-03-16 16:06:46 -0500

    Add files via upload

--- RvsNRCCSRBodeCorrelation.m
+++ RvsNRCCSRBodeCorrelation.m
@@ -1,4 +1,4 @@
-clearvars -except CCEPs
+clearvars
 cd /data/project/NSPMlab/hbriny99/UAB_CCEPdata/CCSR_Resonance_Comparison
 load("CCEPs.mat");
 cd('/data/project/NSPMlab/hbriny99/UAB_CCEPdata/Output');
@@ -9,12 +9,11 @@ for i = 1:length(pt_list)%each pt
     load("CorrAvg.mat")
     load("CorrN1.mat")
     load("CorrN2.mat")
-    load("CorrNrand.mat")
-    load("CorrNull.mat")
+    load("CorrBaseline.mat")
     load("meanRandCorrAvg.mat")
     load("meanRandCorrN1.mat")
     load("meanRandCorrN2.mat")
-    load("meanRandCorrNrand.mat")
+    load("meanRandCorrBaseline.mat")
     index = CCEPs.(pt).index;
     m = nanmean(index);
     f = find(isnan(m));
@@ -36,15 +35,10 @@ for i = 1:length(pt_list)%each pt
     SplitCorr.(pt).N2.R = RccepCorrN2;
     SplitCorr.(pt).N2.NR = NRccepCorrN2;
 
-    RccepCorrNrand = CorrNrand(index==1);
-    NRccepCorrNrand = CorrNrand(index==0);
-    SplitCorr.(pt).Rand.R = RccepCorrNrand;
-    SplitCorr.(pt).Rand.NR = NRccepCorrNrand;
-
-    RccepCorrNull = CorrNull(index==1);
-    NRccepCorrNull = CorrNull(index==0);
-    SplitCorr.(pt).Null.R = RccepCorrNull;
-    SplitCorr.(pt).Null.NR = NRccepCorrNull;
+    RccepCorrBaseline = CorrBaseline(index==1);
+    NRccepCorrBaseline = CorrBaseline(index==0);
+    SplitCorr.(pt).Baseline.R = RccepCorrBaseline;
+    SplitCorr.(pt).Baseline.NR = NRccepCorrBaseline;
 
     RccepRandCorrAvg = meanRandCorrAvg(index==1);
     NRccepRandCorrAvg = meanRandCorrAvg(index==0);
@@ -61,11 +55,6 @@ for i = 1:length(pt_list)%each pt
     SplitCorr.(pt).RandN2.R = RccepRandCorrN2;
     SplitCorr.(pt).RandN2.NR = NRccepRandCorrN2;
 
-    RccepRandCorrNrand = meanRandCorrNrand(index==1);
-    NRccepRandCorrNrand = meanRandCorrNrand(index==0);
-    SplitCorr.(pt).RandRand.R = RccepRandCorrNrand;
-    SplitCorr.(pt).RandRand.NR = NRccepRandCorrNrand;
-
     SplitCorr.Medians.Avg.R(i) = nanmedian(RccepCorrAvg);
     SplitCorr.Medians.Avg.NR(i) = nanmedian(NRccepCorrAvg);
 
@@ -75,11 +64,8 @@ for i = 1:length(pt_list)%each pt
     SplitCorr.Medians.N2.R(i) = nanmedian(RccepCorrN2);
     SplitCorr.Medians.N2.NR(i) = nanmedian(NRccepCorrN2);
 
-    SplitCorr.Medians.Rand.R(i) = nanmedian(RccepCorrNrand);
-    SplitCorr.Medians.Rand.NR(i) = nanmedian(NRccepCorrNrand);
-
-    SplitCorr.Medians.Null.R(i) = nanmedian(RccepCorrNull);
-    SplitCorr.Medians.Null.NR(i) = nanmedian(NRccepCorrNull);
+    SplitCorr.Medians.Baseline.R(i) = nanmedian(RccepCorrBaseline);
+    SplitCorr.Medians.Baseline.NR(i) = nanmedian(NRccepCorrBaseline);
 
     SplitCorr.Medians.RandAvg.R(i) = nanmedian(RccepRandCorrAvg);
     SplitCorr.Medians.RandAvg.NR(i) = nanmedian(NRccepRandCorrAvg);
@@ -89,9 +75,6 @@ for i = 1:length(pt_list)%each pt
 
     SplitCorr.Medians.RandN2.R(i) = nanmedian(RccepRandCorrN2);
     SplitCorr.Medians.RandN2.NR(i) = nanmedian(NRccepRandCorrN2);
-
-    SplitCorr.Medians.RandRand.R(i) = nanmedian(RccepRandCorrNrand);
-    SplitCorr.Medians.RandRand.NR(i) = nanmedian(NRccepRandCorrNrand);
 end
 z = zeros(i,1);
 o = ones(i,1);
@@ -108,7 +91,7 @@ x1 = ones(1,length(pt_list));
 x2 = x1*2;
 x3 = x1*3;
 x4 = x1*4; 
-y1 = SplitCorr.Medians.Null.R;
+y1 = SplitCorr.Medians.Baseline.R;
 y2 = SplitCorr.Medians.N1.R; 
 y3 = SplitCorr.Medians.N2.R; 
 y4 = SplitCorr.Medians.Avg.R;
@@ -133,7 +116,7 @@ x1 = ones(1,length(pt_list));
 x2 = x1*2;
 x3 = x1*3;
 x4 = x1*4; 
-y1 = SplitCorr.Medians.Null.NR; 
+y1 = SplitCorr.Medians.Baseline.NR; 
 y2 = SplitCorr.Medians.N1.NR;
 y3 = SplitCorr.Medians.N2.NR;
 y4 = SplitCorr.Medians.Avg.NR;
